a6fe6168d8f2c8fc8b0a36a5f776c7ea024cb5d2
me2x <vitali.ema@hotmail.it>
2016-05-10 14:16:57 +0200

    setup

---
+++ common.cpp
@@ -0,0 +1,29 @@
+#include "common.h"
+
+std::string get_search_layer_names(Search_Layers l)
+{
+    switch (l)
+    {
+        case S_CONTROLLER:
+            return "Controller";break;
+        case S_RESOURCE:
+            return "Resource";break;
+        case S_PHYSICAL:
+            return "Physical";break;
+        default:
+            return "Error"; break;
+    }
+}
+
+std::string get_search_type_name(Search_Types t)
+{
+    switch (t)
+    {
+        case TIMING:
+            return "Timing"; break;
+        case PROVA:
+            return "Stocazzo";break;
+        default:
+            return "Error"; break;
+    }
+}
---
+++ common.h
@@ -0,0 +1,14 @@
+enum Search_Layers{
+    S_CONTROLLER,
+    S_RESOURCE,
+    S_PHYSICAL,
+    SEARCH_LAYER_END
+};
+enum Search_Types{
+    TIMING,
+    PROVA,
+    SEARCH_TYPE_END
+};
+
+std::string get_search_layer_names(Search_Layers l);
+std::string get_search_type_name(Search_Types t);
